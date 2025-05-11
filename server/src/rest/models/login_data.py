227dd31449c2188a531844a0fca67ebef30e1656
sergei <vasilevsergei995@gmail.com>
2025-05-11 15:48:54 +0300

    фикс структуры

--- server/src/rest/models/login_data.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from pydantic import BaseModel, EmailStr, Field
-
-
-class LoginData(BaseModel):
-    email: EmailStr
-    password: Annotated[str, Field(min_length=8, max_length=128)]
--- server/src/rest/models/login_response.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class LoginResponse(BaseModel):
-    message: str
-    token: Optional[str] = None
-    account_id: Optional[int] = None
